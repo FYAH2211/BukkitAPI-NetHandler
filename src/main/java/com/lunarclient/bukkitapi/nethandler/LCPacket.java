3d5f51231234ff6bcd8f11a874ea2fbbe4c35a80
Francisco Saldanha <frgsaldanha@gmail.com>
2020-03-11 18:47:03 -0300

    Add bossbar packet

--- src/main/java/com/lunarclient/bukkitapi/nethandler/LCPacket.java
+++ src/main/java/com/lunarclient/bukkitapi/nethandler/LCPacket.java
@@ -49,6 +49,7 @@ public abstract class LCPacket {
         addPacket(21, LCPacketWorldBorderRemove.class);
         addPacket(22, LCPacketWorldBorderUpdate.class);
         addPacket(25, LCPacketGhost.class);
+        addPacket(26, LCPacketBossBar.class);
 
         // shared
         addPacket(26, LCPacketEmoteBroadcast.class);
