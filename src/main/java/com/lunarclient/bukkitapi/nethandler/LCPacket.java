4107ee10170f5e2783ab156cf67dfc69aab82055
Colin McDonald <cmcdonald.main@gmail.com>
2020-04-08 01:18:40 -0400

    Pointless build

--- src/main/java/com/lunarclient/bukkitapi/nethandler/LCPacket.java
+++ src/main/java/com/lunarclient/bukkitapi/nethandler/LCPacket.java
@@ -22,6 +22,7 @@ public abstract class LCPacket {
     private static final Map<Integer, Class> idToClass = new HashMap<>();
 
     static {
+	// server
         addPacket(0, LCPacketClientVoice.class);
         addPacket(16, LCPacketVoice.class);
         addPacket(1, LCPacketVoiceChannelSwitch.class);
