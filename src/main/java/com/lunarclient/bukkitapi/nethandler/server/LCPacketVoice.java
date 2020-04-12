9fcbf6f5baa972aed1f7cd3482f0bcf7533ed046
PrestigePvP <ib4est123@gmail.com>
2020-04-12 09:58:26 -0700

    Commit to the correct LCNetHandler

--- src/main/java/com/lunarclient/bukkitapi/nethandler/server/LCPacketVoice.java
+++ src/main/java/com/lunarclient/bukkitapi/nethandler/server/LCPacketVoice.java
@@ -8,30 +8,54 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
 public final class LCPacketVoice extends LCPacket {
+    /**
+     * The UUID of all the senders, since we're using multiplex
+     * we don't actually know the bytes per user
+     */
+    @Getter private Set<UUID> uuids;
 
-    @Getter private UUID uuid;
-
+    /**
+     * The "smashed" bytes of all the voice.
+     */
     @Getter private byte[] data;
 
     @Override
     public void write(ByteBufWrapper b) {
-        b.writeUUID(this.uuid);
+        b.writeVarInt(uuids.size());
+        uuids.forEach(b::writeUUID);
         writeBlob(b, this.data);
     }
 
     @Override
     public void read(ByteBufWrapper b) {
-        this.uuid = b.readUUID();
+        uuids = new HashSet<>();
+        int size = b.readVarInt();
+        for (int i = 0; i < size; i++) {
+            uuids.add(b.readUUID());
+        }
         this.data = readBlob(b);
     }
 
     @Override
     public void process(LCNetHandler handler) {
         ((LCNetHandlerClient) handler).handleVoice(this);
+    }
+
+    /**
+     * For comparability, will be removed.
+     * @return The 0 index of the sent players.
+     */
+    @Deprecated
+    public UUID getUuid() {
+        return new ArrayList<>(uuids).get(0);
     }
 }
