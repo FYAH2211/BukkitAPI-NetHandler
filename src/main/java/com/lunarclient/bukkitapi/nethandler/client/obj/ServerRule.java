0c3d897907b9f80e934e1b6708687497bfc58242
Ryan Willette <rbrickproductions123@gmail.com>
2020-04-25 03:07:39 -0500

    [proof of concept] Server rule overhaul (aka allow enabling and disabling of mods from server side)

--- src/main/java/com/lunarclient/bukkitapi/nethandler/client/obj/ServerRule.java
+++ src/main/java/com/lunarclient/bukkitapi/nethandler/client/obj/ServerRule.java
@@ -1,52 +1,129 @@
 package com.lunarclient.bukkitapi.nethandler.client.obj;
 
-import lombok.Getter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
-public enum ServerRule {
+public final class ServerRule {
 
-    /**
-     * Whether or not minimap is allowed
-     * Expected value: (String) NEUTRAL or FORCED_OFF
-     */
-    MINIMAP_STATUS("minimapStatus", String.class),
+    public static final ServerRule DEFAULT_RULE = new ServerRule();
 
-    /**
-     * Whether or not the server will store waypoints, instead of the client
-     */
-    SERVER_HANDLES_WAYPOINTS("serverHandlesWaypoints", Boolean.class),
+    private String minimapStatus = "FORCED_OFF";
+    private boolean serverHandlesWaypoints = false;
+    private boolean competitiveGame = false;
+    private boolean shadersDisabled = false;
+    private boolean voiceEnabled = false;
 
-    /**
-     * A warning message will be shown when attempting to disconnect if the current
-     * game is competitive.
-     */
-    COMPETITIVE_GAME("competitiveGame", Boolean.class),
-    
-    /**
-     * If this server forces shaders to be disabled
-     */
-    SHADERS_DISABLED("shadersDisabled", Boolean.class),
-    
-    /**
-     * If this server has enabled voice chat
-     */
-    VOICE_ENABLED("voiceEnabled", Boolean.class);
+    private Map<String, ModSetting> modSettings = new HashMap<>();
 
-    @Getter private String id;
-    @Getter private Class type;
 
-    ServerRule(String id, Class type) {
-        this.id = id;
-        this.type = type;
+    public Map<String, ModSetting> getModSettings() {
+        return modSettings;
     }
 
-    public static ServerRule getRule(String id) {
-        for (ServerRule existing : ServerRule.values()) {
-            if (existing.id.equals(id)) {
-                return existing;
-            }
+    public String getMinimapStatus() {
+        return minimapStatus;
+    }
+
+    public boolean isCompetitiveGame() {
+        return competitiveGame;
+    }
+
+    public boolean isShadersDisabled() {
+        return shadersDisabled;
+    }
+
+    public boolean isVoiceEnabled() {
+        return voiceEnabled;
+    }
+
+    public boolean doesServerHandlesWaypoints() {
+        return serverHandlesWaypoints;
+    }
+
+    public ServerRule setMinimapStatus(String minimapStatus) {
+        this.minimapStatus = minimapStatus;
+        return this;
+    }
+
+    public ServerRule setCompetitiveGame(boolean competitiveGame) {
+        this.competitiveGame = competitiveGame;
+        return this;
+    }
+
+    public ServerRule setShadersDisabled(boolean shadersDisabled) {
+        this.shadersDisabled = shadersDisabled;
+        return this;
+    }
+
+    public ServerRule setVoiceEnabled(boolean voiceEnabled) {
+        this.voiceEnabled = voiceEnabled;
+        return this;
+    }
+
+    public ServerRule setServerHandlesWaypoints(boolean serverHandlesWaypoints) {
+        this.serverHandlesWaypoints = serverHandlesWaypoints;
+        return this;
+    }
+
+    public ServerRule addSetting(String mod, ModSetting modSetting) {
+        this.modSettings.put(mod, modSetting);
+        return this;
+    }
+
+
+    public static class ModSetting {
+        private boolean enabled;
+        private Map<String, Object> properties;
+
+        public ModSetting() {
+        } // for serialization
+
+        public ModSetting(boolean enabled, Map<String, Object> properties) {
+            this.enabled = enabled;
+            this.properties = properties;
         }
 
-        return null;
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public Map<String, Object> getProperties() {
+            return properties;
+        }
+
+        @Override
+        public String toString() {
+            return "ModSetting{" +
+                    "enabled=" + enabled +
+                    ", properties=" + properties +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ModSetting that = (ModSetting) o;
+            return enabled == that.enabled &&
+                    Objects.equals(properties, that.properties);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(enabled, properties);
+        }
     }
 
+    @Override
+    public String toString() {
+        return "ServerRule{" +
+                "minimapStatus='" + minimapStatus + '\'' +
+                ", serverHandlesWaypoints=" + serverHandlesWaypoints +
+                ", competitiveGame=" + competitiveGame +
+                ", shadersDisabled=" + shadersDisabled +
+                ", voiceEnabled=" + voiceEnabled +
+                ", modSettings=" + modSettings +
+                '}';
+    }
 }
