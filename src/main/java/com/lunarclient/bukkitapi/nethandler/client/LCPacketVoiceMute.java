07055fc1d635fdc04c50aa6fd08a8711ae32f46b
PrestigePvP <ib4est123@gmail.com>
2020-05-09 18:39:58 -0700

    Allow mute packet to take a volume

--- src/main/java/com/lunarclient/bukkitapi/nethandler/client/LCPacketVoiceMute.java
+++ src/main/java/com/lunarclient/bukkitapi/nethandler/client/LCPacketVoiceMute.java
@@ -13,21 +13,30 @@ public final class LCPacketVoiceMute extends LCPacket {
 
     @Getter private UUID muting;
 
+    @Getter private int volume;
+
     public LCPacketVoiceMute() {
     }
 
     public LCPacketVoiceMute(UUID muting) {
+        this(muting, 0);
+    }
+
+    public LCPacketVoiceMute(UUID muting, int volume) {
         this.muting = muting;
+        this.volume = volume;
     }
 
     @Override
     public void write(ByteBufWrapper b) throws IOException {
         b.writeUUID(muting);
+        b.writeVarInt(volume);
     }
 
     @Override
     public void read(ByteBufWrapper b) throws IOException {
         this.muting = b.readUUID();
+        this.volume = b.readVarInt();
     }
 
     @Override
